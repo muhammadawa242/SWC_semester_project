8a2966401446f0834722d2eae959f93fc42eac4c
Muhammad Awais <muhammadawwa242@gmai.cvom>
2024-01-14 13:18:02 +0500

    stories initial look

--- client/src/Components/Middle/Middle.jsx
+++ client/src/Components/Middle/Middle.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 import './Middle.css'
 // import Picture from '../../assets/profile-8.jpg'
 import PostShare from '../PostShare/PostShare'
@@ -9,56 +8,24 @@ import { useSelector } from 'react-redux'
 function Middle() {
     const aws = useSelector((state) => state.awsPath);
     const imgPath = useSelector((state) => state.user.picturePath);
+    const stories = useSelector((state) => state.stories);
     const Picture = aws+imgPath;
 
   return (
     <div className="middle">   
         {/* Stories */}
         <div className="stories">
-            <div className="story">
-                <div className="profile-photo">
-                    <a href="">
-                        <img src={Picture}/>
-                    </a>
-                </div>
-                <p className="name">Your Story</p>
-            </div>
-
+            {/* Required: add some tag that plays the video provided in its src or url */}
+            {/* aws+stories.videoPath   ... gives the url for the video stored on cloud */}
             <div className="story">
                 <div className="profile-photo">
                     <img src={Picture}/>
                 </div>
-                <p className="name">Your Story</p>
-            </div>
-
-            <div className="story">
-                <div className="profile-photo">
-                    <img src={Picture}/>
-                </div>
-                <p className="name">Your Story</p>
-            </div>
-
-            <div className="story">
-                <div className="profile-photo">
-                    <img src={Picture}/>
-                </div>
-                <p className="name">Your Story</p>
-            </div>
-
-            <div className="story">
-                <div className="profile-photo">
-                    <img src={Picture}/>
-                </div>
-                <p className="name">Your Story</p>
-            </div>
-
-            <div className="story">
-                <div className="profile-photo">
-                    <img src={Picture}/>
-                </div>
-                <p className="name">Your Story</p>
+                <p className="name" style={{ color: 'black' }}><b>Required: </b>add some tag here that plays the video provided in its src or url</p>
             </div>
         </div>
+
+
         {/* POst Share Component */}
         <PostShare />
         <div className="feeds">
